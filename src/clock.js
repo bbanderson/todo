96bb8e24597d5aeed7c0e2fad92543b1000d479a
bbanderson <tombyun@naver.com>
2020-05-03 10:42:42 +0900

    Modify const TIME which is mistaken

--- src/clock.js
+++ src/clock.js
@@ -2,9 +2,9 @@ const CLOCK_CONTAINER = document.querySelector(".js-clock");
 const CLOCK = CLOCK_CONTAINER.querySelector("h1");
 const DATE_CONTAINER = document.querySelector(".js-date");
 const DATE = DATE_CONTAINER.querySelector("h3");
-const TIME = new Date();
 
 function paintTime() {
+    const TIME = new Date();
     CLOCK.innerHTML = 
     `${TIME.getHours() < 10 ? `0${TIME.getHours()}` : TIME.getHours()} : ${TIME.getMinutes() < 10 ? `0${TIME.getMinutes()}` : TIME.getMinutes()} : ${TIME.getSeconds() < 10 ? `0${TIME.getSeconds()}` : TIME.getSeconds()}`;
     DATE.innerHTML = `${TIME.getFullYear()} . ${TIME.getMonth()+1} . ${TIME.getDate()}`;
