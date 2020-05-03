3c4c1fd07e13a7cad69cdd3a856d14b11e1795df
bbanderson <tombyun@naver.com>
2020-05-03 13:58:53 +0900

    Add responsiveness

--- src/js/coords.js
+++ src/js/coords.js
@@ -1,10 +1,20 @@
 const API_KEY = "2ddad7f301e34d29f302e40dd892bc4a";
 const COORDS = "coords";
+const WEATHER_CONTAINER = document.querySelector(".js-weather")
+
+// ☀️🌤⛅️🌥☁️
+function getWeatherIcon(icon) {
+    const img = new Image();
+    img.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+    return img
+}
 
 function fetchWeather(lat, lon) {
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`).then(response => {
         response.json().then(result => {
-            console.log(result)
+            WEATHER_CONTAINER.querySelector(".city").innerHTML = result.name;
+            WEATHER_CONTAINER.querySelector(".temperature").innerHTML = `${Math.round(result.main.temp)} ℃`;
+            WEATHER_CONTAINER.querySelector(".weather").prepend(getWeatherIcon(result.weather[0].icon));
         })
     })
 }
